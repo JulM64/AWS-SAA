4f7e3fdd4ed0f18659ee84c1b79ddf2c1b7b892f
JulM64 MVENG <julienmveng6@gmail.com>
2025-03-06 12:52:46 +0000

    ruby file changing

--- s3/sdk/ruby/s3.rb
+++ s3/sdk/ruby/s3.rb
@@ -3,4 +3,32 @@ require 'pry'
 require 'securerandom'
 
 bucket_name = ENV['BUCKET_NAME']
-puts bucket_name
+region = 'us-east-1'
+
+client = Aws::S3::Client.new
+
+resp = client.create_bucket({
+    bucket: bucket_name,
+    create_bucket_configuration: {
+        location_constraint: region
+    },
+})
+binding.pry
+
+number_of_files = 1+ rand(6)
+puts "number_of_files: #{number_of_files}"
+number_of_files.times.each do |i|
+    puts"i: #{i}"
+    filename = "file_#{i}.txt"
+    output_path = "/tmp/#{filename}"
+    file.open(output_path,"w") do |f|
+        f.write securerandom.uuid
+    end
+    file.open(output_path,"rb") do |f|
+        s3.put_object(
+            bucket: 'bucket_name',
+            key: 'filename', 
+            body: 'f'
+        )
+    end
+end
