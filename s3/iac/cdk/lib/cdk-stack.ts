d89dd012d77f2fb6466068d6d355d7e4c0ed6d50
JulM64 MVENG <julienmveng6@gmail.com>
2025-03-22 18:47:23 +0000

    simple s3 cdk example

--- s3/iac/cdk/lib/cdk-stack.ts
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -3,8 +3,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
-  constructor(scope: cdk.App, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    //const bucket = new s3.Bucket(scope, 'MyjcBucket');
+    const bucket = new s3.Bucket(this, 'MyjcBucket');
   }
 }
