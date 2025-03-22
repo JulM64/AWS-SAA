acda5f29fe6e4175ed26fdeebe2532dbf70161c6
JulM64 MVENG <julienmveng6@gmail.com>
2025-03-22 17:50:15 +0000

    cdk stack modified

--- s3/iac/cdk/lib/cdk-stack.ts
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -3,15 +3,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: cdk.App, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    //const bucket = new s3.Bucket(scope, 'MyjcBucket');
   }
 }
