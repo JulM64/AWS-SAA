99697200b92ae1a4bb45545a81ff3edbf58e4893
JulM64 MVENG <julienmveng6@gmail.com>
2025-04-26 13:13:56 +0000

    app modified

---
+++ react-app/file-upload-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fileuploadapp29559041": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
